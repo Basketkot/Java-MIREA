a54a7c178396afed2d54185a5eb6c44ed8fe170f
Ivan Kotelkov <54940261+Basketkot@users.noreply.github.com>
2022-12-21 14:09:55 +0300

    Update TestAll.java

--- src/ru/mirea/task2/TestAll.java
+++ src/ru/mirea/task2/TestAll.java
@@ -2,15 +2,15 @@ package ru.mirea.task2;
 
 public class TestAll {
     public static void main(String[] args) {
-        Shape shapeObject = new Shape(4, 13);  // Класс Shape
+        Shape shapeObject = new Shape(12, 33);  // Класс Shape
         System.out.println(shapeObject.toString());
-        Ball objectBall = new Ball("Красный", 9);  // Класс Ball
+        Ball objectBall = new Ball("Yellow", 5);  // Класс Ball
         System.out.println(objectBall.toString());
-        Book objectBook = new Book("Война миров Z", "Макс", 666);  // Класс Book
+        Book objectBook = new Book("Getting thinks done", "David Allen", 416);  // Класс Book
         System.out.println(objectBook.toString());
-        Dog objectDog1 = new Dog("Фостер", 7);  // Класс Dog
-        Dog objectDog2 = new Dog("Джексон", 5);
-        Dog objectDog3 = new Dog("Молли", 3);
+        Dog objectDog1 = new Dog("Bob", 3);  // Класс Dog
+        Dog objectDog2 = new Dog("Carl", 5);
+        Dog objectDog3 = new Dog("Rebecca", 7);
         System.out.println(objectDog1.toString());
         System.out.println("Dog: name=" + objectDog2.getName() + ", age=" + objectDog2.getAge());
         System.out.println("Dog: name=" + objectDog3.getName() + ", people_age=" + objectDog3.getPeopleAge());
